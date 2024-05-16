7e3da94b733f1ce525353c3828ff47300fabe1be
Sonja Lorscheidt <sonja.lorscheidt@gmail.com>
2024-05-16 17:02:53 +0200

    Raw version finalised

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,8 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
+
+  def movies_not_in_list
+    Movie.all.reject { |movie| self.movies.include?(movie)}
+  end
 end
