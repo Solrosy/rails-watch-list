6257d51a9d7b50e2eed7574f714564a3f7473a38
Sonja Lorscheidt <sonja.lorscheidt@gmail.com>
2024-05-17 17:58:00 +0200

    Done!

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,11 @@ movie3 = Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Buka
 movie4 = Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 puts "Movies created"
 
-list1 = List.create(name: "Sonja's Movies")
-List.create(name: "Linneas's Movies")
-List.create(name: "Tim's Movies")
+
+
+list1 = List.create(name: "Sonjas Movies")
+List.create(name: "Linneass Movies")
+List.create(name: "Tims Movies")
 puts "Lists created"
 
 
