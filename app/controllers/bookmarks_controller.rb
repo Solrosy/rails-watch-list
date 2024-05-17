8ff5889a2906d34911a4a7f0aa0ed84e123b63e8
Solros <60941951+Solrosy@users.noreply.github.com>
2024-05-17 22:21:45 +0200

    sorted

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,16 +1,23 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+
+  def new
+    @bookmar = Bookmark.new
+  end
+
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @movie = Movie.find(params["bookmark"]["movie"])
-    @list = List.find(params["list_id"])
-    @bookmark.movie = @movie
     @bookmark.list = @list
-    @bookmark.save
-    redirect_to list_path(@list)
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to list_path(@bookmark.list), status: :see_other
   end
@@ -18,6 +25,14 @@ class BookmarksController < ApplicationController
 
   private
   def bookmark_params
-      params.require(:bookmark).permit(:comment)
+      params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,11 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :update]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
     @bookmark = Bookmark.new
   end
 
@@ -15,22 +16,24 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to @list
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def update
-    @list = List.find(params[:id])
     if @list.update(list_params)
-      redirect_to @list, notice: 'List was successfully updated.'
+      redirect_to @list
     else
       render :update
     end
   end
 
   private
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name, :photo)
